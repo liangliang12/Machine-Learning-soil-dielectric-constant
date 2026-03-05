d6a352c33046fe22e23f542b8f60c3f4091ebf59
gaoxiaoliang <33344623+liangliang12@users.noreply.github.com>
2026-03-05 14:20:06 +0800

    Fix formatting and update comments in example script

--- Ssc_Retrieval_Model/SscRetrieval_Cband_Real/SscRetrieval_Cband_Real_model_example.py
+++ Ssc_Retrieval_Model/SscRetrieval_Cband_Real/SscRetrieval_Cband_Real_model_example.py
@@ -14,11 +14,11 @@ import matplotlib.pyplot as plt
 ############################################################################################################
 ## The introduction of model input parameters 
 # real:      imaginary part of soil dielectric constant
-# Frequency: Microwave frequency
-# Sandy:     Soil sand fraction
-# Clay:      Soil clay fraction
-# rou_s:     Soil specific dendity 
-# rou_b:     Soil bulk dendity 
+# Frequency: Microwave frequency（5-6 GHz）
+# Sandy:     Soil sand fraction（0-1）
+# Clay:      Soil clay fraction（0-1）
+# rou_s:     Soil specific dendity(g/cm³)  
+# rou_b:     Soil bulk dendity(g/cm³)  
 #
 ##The introduction of model output parameters 
 # Salinity: the predicted Soil salinity content
@@ -67,3 +67,4 @@ y_predict = model_RF.predict(X)
 #moisture: soil moisture content, salinity: soil salinity content, frequency: microwave frequency,
 #Sandy: soil sandy fraction, Clay: soil clay fraction, rou_s: soil specific density, rou_b: soil bulk density
 #the unit of these input parameters are: moisture(m³/m³), salinity(g/kg), frequency(GHz), Sandy(%), Clay(%), rou_s(g/m³), rou_b(g/cm³),
+
